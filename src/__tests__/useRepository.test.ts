a9048973e2197b45f3657ab3a57eef783914bf72
Yannick Fricke <yannickfricke@googlemail.com>
2020-09-13 05:40:57 +0200

    refactor(hooks): renamed the useArrayRepository hook to useRepository

--- src/__tests__/useRepository.test.ts
+++ src/__tests__/useRepository.test.ts
@@ -1,7 +1,175 @@
 import { useRepository } from '../useRepository';
+import { Identifiable } from '../Identifiable';
 
-describe('useRepository', () => {
+interface ITestEntry extends Identifiable {
+    name: string;
+}
+
+describe('useArrayRepository', () => {
+    let entries: ITestEntry[];
+    const setEntries = (newEntries: ITestEntry[]) => {
+        entries = newEntries;
+    };
+
+    beforeEach(() => {
+        entries = [];
+    });
+
+    const testEntry: ITestEntry = {
+        id: 1,
+        name: 'test entry',
+    };
+
     it('should be defined', () => {
         expect(useRepository).toBeDefined();
+    });
+
+    describe('find', () => {
+        it('should find a single entry', () => {
+            entries.push(testEntry);
+
+            const repository = useRepository(entries, setEntries);
+
+            const result = repository.find(1);
+
+            expect(result).toBeDefined();
+            expect(result).toStrictEqual(testEntry);
+        });
+
+        it('should find a single entry by the given props', () => {
+            entries.push(testEntry);
+
+            const repository = useRepository(entries, setEntries);
+
+            const result = repository.findOneBy({
+                id: 1,
+            });
+
+            expect(result).toBeDefined();
+            expect(result).toStrictEqual(testEntry);
+        });
+
+        it('should find multiple entries', () => {
+            entries.push(testEntry);
+            entries.push({
+                id: 2,
+                name: testEntry.name,
+            });
+            entries.push({
+                id: 3,
+                name: 'Finish tests',
+            });
+
+            const repository = useRepository(entries, setEntries);
+
+            const result = repository.findBy({
+                name: testEntry.name,
+            });
+
+            expect(result).toHaveLength(2);
+            expect(result).toContainEqual(testEntry);
+            expect(result).toContainEqual({
+                id: 2,
+                name: testEntry.name,
+            });
+        });
+    });
+
+    describe('insert new entries', () => {
+        it('should not insert an entry twice', () => {
+            entries.push(testEntry);
+
+            const repository = useRepository(entries, setEntries);
+            repository.insert(testEntry);
+
+            expect(entries).toHaveLength(1);
+        });
+
+        it('should insert new entries', () => {
+            const repository = useRepository(entries, setEntries);
+            repository.insert(testEntry);
+
+            expect(entries).toHaveLength(1);
+        });
+    });
+
+    describe('update entries', () => {
+        it('should update entries', () => {
+            entries.push(testEntry);
+
+            const repository = useRepository(entries, setEntries);
+
+            repository.update(testEntry.id, {
+                name: 'TDD with jest rocks!',
+            });
+
+            expect(entries).toHaveLength(1);
+            expect(
+                entries,
+            ).toStrictEqual([
+                {
+                    id: testEntry.id,
+                    name: 'TDD with jest rocks!',
+                },
+            ]);
+        });
+
+        it('should do nothing, when no entry with the given id was found', () => {
+            entries.push({
+                id: 2,
+                name: 'TDD with jest rocks!',
+            });
+
+            const repository = useRepository(entries, setEntries);
+
+            repository.update(1, {
+                name: 'New name',
+            });
+
+            expect(entries).toHaveLength(1);
+            expect(entries).toStrictEqual([
+                {
+                    id: 2,
+                    name: 'TDD with jest rocks!',
+                },
+            ]);
+        });
+    });
+
+    describe('remove entries', () => {
+        it('should remove entry by id', () => {
+            entries.push(testEntry);
+
+            const repository = useRepository(entries, setEntries);
+            repository.remove(testEntry.id);
+
+            expect(entries).toHaveLength(0);
+        });
+
+        it('should remove entries by props', () => {
+            entries.push(testEntry);
+            entries.push(testEntry);
+            entries.push({
+                id: 2,
+                name: 'Unit testing with jest',
+            });
+
+            const repository = useRepository(entries, setEntries);
+
+            repository.removeBy({
+                name: 'test entry',
+            });
+
+            expect(entries).toHaveLength(1);
+        });
+
+        it('should remove all entries', () => {
+            entries.push(testEntry);
+
+            const repository = useRepository(entries, setEntries);
+            repository.removeAll();
+
+            expect(entries).toHaveLength(0);
+        });
     });
 });
--- src/useRepository.ts
+++ src/useRepository.ts
@@ -1,3 +1,103 @@
-export const useRepository = () => {
+import { Identifiable } from './Identifiable';
+import find from 'lodash/find';
+import filter from 'lodash/filter';
+import concat from 'lodash/concat';
+import reject from 'lodash/reject';
+import { IRepository } from './IRepository';
 
+export const useRepository = <T extends Identifiable>(values: T[], setValues: (newValue: T[]) => void): IRepository<T> => {
+    return {
+        /**
+         * Returns the entry with the given id.
+         * Undefined will be returned when no entry was found.
+         *
+         * @param {string} id The unique identifier of the entry
+         * @return {T | undefined} The found entry or undefined when no entry was found
+         */
+        find: function(id: Identifiable['id']) {
+            return this.findOneBy({
+                id,
+            } as Partial<T>) as T | undefined;
+        },
+
+        /**
+         * Finds one entity with the given params
+         *
+         * @param {Partial<T>} params The properties which should match
+         * @return {T | undefined} The found entry or undefined when no entry was found
+         */
+        findOneBy: function(params: Partial<T>) {
+            return find(values, params) as T | undefined;
+        },
+
+        /**
+         * Returns all entities who properties match the params
+         *
+         * @param {Partial<T>} params The params which should match
+         * @return {T[]} The found entries
+         */
+        findBy: function(params: Partial<T>) {
+            return filter(values, params) as T[];
+        },
+
+        /**
+         * Inserts a new entity
+         *
+         * @param {T} value The entity which should be added
+         */
+        insert: function(value: T) {
+            if (this.find(value.id) !== undefined) {
+                return;
+            }
+
+            const newValues = concat(
+                values,
+                value,
+            );
+            setValues(newValues);
+        },
+
+        /**
+         * Updates the entity with the given id and the given props
+         *
+         * @param id The unique identifier of the entity which should be updated
+         * @param newProps The new properties for the entry
+         */
+        update: function(id: Identifiable['id'], newProps: Partial<T>) {
+            if (this.find(id) === undefined) {
+                return;
+            }
+
+            const newValues = values.map(entry => entry.id !== id ? entry : {
+                ...entry,
+                ...newProps,
+            });
+            setValues(newValues);
+        },
+
+        /**
+         * Removes the entry with the given id from the values
+         *
+         * @param id The unique identifier of the entry
+         */
+        remove: function(id: Identifiable['id']) {
+            setValues(values.filter(entry => entry.id !== id));
+        },
+
+        /**
+         * Removes entries by the given props
+         *
+         * @param props The props which should be matched
+         */
+        removeBy: function(props: Partial<T>) {
+            setValues(reject(values, props) as T[]);
+        },
+
+        /**
+         * Removes all entries
+         */
+        removeAll: function() {
+            setValues([]);
+        },
+    };
 };
