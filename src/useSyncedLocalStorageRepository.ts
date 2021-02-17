ee5f71c6909a630091c0c26fab0fcce213167be5
Yannick Fricke <yannickfricke@googlemail.com>
2021-02-17 06:56:35 +0100

    feat(usesyncedlocalstoragerepository): added the useSyncedLocalStorageRepository hook
    The useSyncedLocalStorageRepository hook uses the synchronized local storage. Every change which is
    made against the repository will be reflected in other browser windows.

---
+++ src/useSyncedLocalStorageRepository.ts
@@ -0,0 +1,13 @@
+import { useSyncedLocalStorage } from '@yannickfricke/use-local-storage';
+
+import { IRepository } from './IRepository';
+import { IEntityType } from './types';
+import { useRepository } from './useRepository';
+
+export const useSyncedLocalStorageRepository = <T extends IEntityType>(
+    tableName: string,
+): IRepository<T> => {
+    const [value, setValue] = useSyncedLocalStorage<T[]>(tableName, []);
+
+    return useRepository(value, setValue);
+};
